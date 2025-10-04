94babd2513ff19d9ba02ebccdbd14e82e24a5868
Myroslav Polishchuk <myrospolil@gmail.com>
2025-10-04 23:52:34 +0100

    Add Maper for Product

---
+++ src/main/java/com/myroslav/cosmickitties/mapper/ProductMapper.java
@@ -0,0 +1,14 @@
+package com.myroslav.cosmickitties.mapper;
+
+import com.myroslav.cosmickitties.domain.Product;
+import com.myroslav.cosmickitties.dto.ProductDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+
+    ProductDTO toDto(Product product);
+    Product toDomain(ProductDTO dto);
+}
