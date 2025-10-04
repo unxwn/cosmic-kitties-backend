cd88fe59377dd1745fd0d685bf556a11cfd77027
Myroslav Polishchuk <myrospolil@gmail.com>
2025-10-04 23:35:05 +0100

    Add Service for Products

---
+++ src/main/java/com/myroslav/cosmickitties/service/IProductService.java
@@ -0,0 +1,13 @@
+package com.myroslav.cosmickitties.service;
+
+import com.myroslav.cosmickitties.dto.ProductDTO;
+
+import java.util.List;
+
+public interface IProductService {
+    ProductDTO create(ProductDTO dto);
+    ProductDTO getById(Long id);
+    List<ProductDTO> getAll();
+    ProductDTO update(Long id, ProductDTO dto);
+    void delete(Long id);
+}
