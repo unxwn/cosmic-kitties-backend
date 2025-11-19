ade52253669b44879df94a2bc25ddb7ed8334c78
Myroslav Polishchuk <myrospolil@gmail.com>
2025-11-19 22:53:18 +0000

    feat: add FeatureNotAvailableException for disabled features

--- src/main/java/com/myroslav/cosmickitties/feature/FeatureNotAvailableException.java
+++ src/main/java/com/myroslav/cosmickitties/feature/FeatureNotAvailableException.java
@@ -1,4 +1,7 @@
 package com.myroslav.cosmickitties.feature;
 
-public class FeatureNotAvailableException {
+public class FeatureNotAvailableException extends RuntimeException {
+    public FeatureNotAvailableException(String featureKey) {
+        super("Feature '" + featureKey + "' is notf available");
+    }
 }
