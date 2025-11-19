503b41588fb6e5a428ab19958e50595cf9ba09bc
Myroslav Polishchuk <myrospolil@gmail.com>
2025-11-19 23:05:21 +0000

    feat: add FeatureToggle annotation

--- src/main/java/com/myroslav/cosmickitties/feature/FeatureToggle.java
+++ src/main/java/com/myroslav/cosmickitties/feature/FeatureToggle.java
@@ -1,4 +1,14 @@
 package com.myroslav.cosmickitties.feature;
 
-public class FeatureToggle {
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FeatureToggle {
+    /**
+     * Logical name of the feature key WITHOUT the 'feature.' prefix and without '.enabled'.
+     * Example: for property feature.cosmo-cats.enabled use @FeatureToggle("cosmo-cats")
+     */
+    String value();
 }
