83cd45116bd9f40560e5f8c7f62a62f27b850e3a
Myroslav Polishchuk <myrospolil@gmail.com>
2025-11-19 23:13:11 +0000

    feat: add CosmoCatService with @FeatureToggle protection

--- src/main/java/com/myroslav/cosmickitties/service/CosmoCatService.java
+++ src/main/java/com/myroslav/cosmickitties/service/CosmoCatService.java
@@ -1,4 +1,19 @@
 package com.myroslav.cosmickitties.service;
 
+import com.myroslav.cosmickitties.feature.FeatureToggle;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Example service - returns a list of Cosmo Cats.
+ * Protected by the feature toggle 'cosmo-cats' (property: feature.cosmo-cats.enabled).
+ */
+@Service
 public class CosmoCatService {
+
+    @FeatureToggle("cosmo-cats")
+    public List<String> getCosmoCats() {
+        return List.of("Rocket the Voyager", "Stella the Starclimber", "Nebula");
+    }
 }
