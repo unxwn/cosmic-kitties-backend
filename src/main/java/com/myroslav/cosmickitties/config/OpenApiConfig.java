842ba0357b62fdba936cfeae3d0570f6546a5d61
Myroslav Polishchuk <myrospolil@gmail.com>
2025-12-16 19:05:22 +0000

    docs: configure OpenAPI security schemes for Swagger UI

---
+++ src/main/java/com/myroslav/cosmickitties/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.myroslav.cosmickitties.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("apiKey", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-API-KEY")
+                                .description("API Key for authentication"))
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT Bearer token authentication")));
+    }
+}
